49755c57ab34e8d5652f0773f53140358d1242e7
LuisL <dev.eng.luisluna@gmail.com>
2023-11-23 14:54:00 +0000

    version 2.0 comentado

--- src/app.py
+++ src/app.py
@@ -6,11 +6,23 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+# Importaciones System): Módulo para interactuar con el sistema operativo.
+# Flask: Clase principal para crear aplicaciones web con Flask.
+# request: Objeto para manejar las solicitudes HTTP.
+# jsonify: Función para convertir objetos Python a respuestas JSON.
+# url_for: Función para construir URLs.
+# CORS: Clase para habilitar CORS (Cross-Origin Resource Sharing).
+# Protocolos de Seguridad: APIException: Clase para manejar excepciones personalizadas.
+# Base de datos: generate_sitemap: Función para generar un sitemap.
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
+
+# Creamos una instancia de la aplicación Flask.
+# app.url_map.strict_slashes = False: Configuración para permitir rutas con o sin barra inclinada 
+# al final.
+# CORS(app): Habilita CORS en la aplicación para permitir solicitudes desde diferentes dominios.
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
@@ -21,9 +33,11 @@ def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+# Se define una ruta / que devuelve un sitemap generado a partir de los endpoints de la aplicación.
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
@@ -34,11 +48,52 @@ def handle_hello():
         "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+
+    
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+# /members (GET): Devuelve información sobre todos los miembros de la familia Jackson.
+def get_one_member(member_id):
+    member = jackson_family.get_member(member_id)
+    return jsonify(member), 200
+# /member/<int:member_id> (GET): Devuelve información sobre un miembro específico de la familia
+#  Jackson.
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    body_last_name = request.json.get("last_name")
+    body_name = request.json.get("first_name")
+    body_age = request.json.get("age")
+    body_id = request.json.get("id")
+    body_lucky_numbers = request.json.get("lucky_numbers")
+    member = {
+        "id": body_id or jackson_family._generateId(),
+        "first_name": body_name,
+        "last_name": body_last_name,
+        "age": body_age,
+        "lucky_numbers": body_lucky_numbers,
+    }
+    jackson_family.add_member(member)
+    return jsonify(None), 200
+# /member (POST): Agrega un nuevo miembro a la familia Jackson con la información proporcionada en el cuerpo de la solicitud JSON.
+
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_one_member(member_id):
+    member = jackson_family.delete_member(member_id)
+    return jsonify({"done": True}), 200
+# /member/<int:member_id> (DELETE): Elimina un miembro específico de la familia Jackson.
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
+# Ejecución de la aplicación:
+# El bloque if __name__ == '__main__': asegura que el servidor se inicie solo si se ejecuta el 
+# script directamente.
+# El número de puerto se obtiene de la variable de entorno PORT o se utiliza el valor predeterminado
+#  3000.
+# La aplicación se ejecuta en el host '0.0.0.0', haciendo que sea accesible desde cualquier dirección IP en la red, 
+# y se habilita el modo de depuración (debug=True).
--- src/datastructures.py
+++ src/datastructures.py
@@ -9,28 +9,58 @@ update this file to implement the following already declared methods:
 from random import randint
 
 class FamilyStructure:
-    def __init__(self, last_name):
+   def __init__(self, last_name):
         self.last_name = last_name
+        self._members = [{
+                "id": self._generateId(),
+                "first_name": "Jhon",
+                "last_name": self.last_name,
+                "age": 33,
+                "lucky_numbers": [7, 13, 22] },
+# __init__: Este es el constructor de la clase. Inicializa una instancia de la familia con un 
+# apellido (last_name) y una lista de miembros iniciales con información de nombre, apellido, 
+# edad y números de la suerte.
+            {
+                "id": self._generateId(),
+                "first_name": "Jane",
+                "last_name": self.last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3] },
 
-        # example list of members
-        self._members = []
+            {
+                "id": self._generateId(),
+                "first_name": "Jimmy",
+                "last_name": self.last_name,
+                "age": 5,
+                "lucky_numbers": [1] }]
+# _generateId: Este es un método interno utilizado para generar identificadores aleatorios para 
+# los miembros. 
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
+   # read-only: Use this method to generate random members ID's when adding members into the list
     def add_member(self, member):
         # fill this method and update the return
         pass
+# add_member: Este método debe implementarse para agregar un nuevo miembro a la lista _members.
+# El nuevo miembro se proporciona como argumento al método.
 
     def delete_member(self, id):
         # fill this method and update the return
         pass
+# delete_member: Este método debe implementarse para eliminar un miembro de la lista _members según 
+# el ID proporcionado como argumento.
 
     def get_member(self, id):
         # fill this method and update the return
         pass
+# get_member: Este método debe implementarse para obtener información sobre un miembro específico de
+# la familia según el ID proporcionado como argumento.
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+# get_all_members: Este método devuelve la lista completa de miembros de la familia. Ya está implementado 
+# y no necesita cambios.
